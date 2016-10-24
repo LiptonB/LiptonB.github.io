dfdfbae931dbdf979f80afe83f984354124ce46d
Ben Lipton <ben.lipton@gmail.com>
2016-10-24 15:46:41 -0400

    Fix paths to match previous ones

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,11 +22,15 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>[^.]*)'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -36,5 +40,5 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-MD_EXTENSIONS = [TocExtension(), CodeHiliteExtension(css_class='highlight')]
+MD_EXTENSIONS = [TocExtension(), CodeHiliteExtension(css_class='highlight', guess_lang=False)]
 THEME = '../pelican-themes/alchemy/alchemy'
